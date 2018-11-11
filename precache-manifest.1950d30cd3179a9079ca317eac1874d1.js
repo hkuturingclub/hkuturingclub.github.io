ffe128b4ba4653c0d029bbac47e3578af585d995
Waqas Ali <waqas.abbasi@outlook.com>
2018-11-11 19:51:27 +0800

    Updates

---
+++ precache-manifest.1950d30cd3179a9079ca317eac1874d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "943c7f2eaf19f230296b",
+    "url": "/static/js/main.943c7f2e.chunk.js"
+  },
+  {
+    "revision": "0b0e7ccaeafa5a19a4c7",
+    "url": "/static/js/1.0b0e7cca.chunk.js"
+  },
+  {
+    "revision": "943c7f2eaf19f230296b",
+    "url": "/static/css/main.ce136813.chunk.css"
+  },
+  {
+    "revision": "20aad5feaee842016812b613afed5fd1",
+    "url": "/index.html"
+  }
+];
